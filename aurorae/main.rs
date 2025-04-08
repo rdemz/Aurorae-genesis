5054b2e59bd842719c895b76033d57d9760e1bbe
rdemz <r.demeillez@live.fr>
2025-04-08 10:35:52 +0200

    Update main.rs

--- aurorae/main.rs
+++ aurorae/main.rs
@@ -21,7 +21,7 @@ mod explorer;
 mod crawler;
 mod security;
 
-use crate::autonomy::core::AuroraeCore;
+use crate::autonomy::AuroraeCore;
 use crate::founder_income::set_founder_address;
 use crate::brain::boot_brain;
 use crate::learning::scan_feed_and_learn;
